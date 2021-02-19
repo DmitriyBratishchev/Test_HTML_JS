50b3dcc2dd6c1b7e752fdf67dddd036c0eae25d1
Dmitriy Bratishchev <dmitriy-1008@yandex.ru>
2021-02-19 16:21:14 +0300

    add 2 generator

--- JS/brackets.js
+++ JS/brackets.js
@@ -4,16 +4,18 @@ import {generator1} from './generator1.js';
 
 (function() {
 
+  let bracketCouple = ["()", "[]", "{}"];
   let n = 7;
 
   // Минимально-достаточная функция для решения задачи генерации валидной строки из скобок
   
   let result = generator(n);
-  test(result);
-  
+  testThis(result);
+  testThis('{[](})(){{}[]}')
+
   function generator(n) {
 
-    let bracketCouple = ["()", "[]", "{}"];
+    
     let result = '';
   
     for (let i = 0; i < n; i++) {
@@ -22,6 +24,7 @@ import {generator1} from './generator1.js';
       let simbol = randomNam(0, bracketCouple.length - 1)
   
       result = result.substring(0, position) + bracketCouple[simbol] + result.substr(position);
+      console.log(result);
     }
   
     console.log('result: ', result);
@@ -31,19 +34,33 @@ import {generator1} from './generator1.js';
   function randomNam(min, max) {
     let rand = min + Math.random() * (max + 1 - min);
     return Math.floor(rand);
+
+
   }
 
-  //Варианты решения. с использованием усложнений.
-
-  let result1 = generator1(n);
-  test(result1);
-
-  let result2 = generator2(n);
-  test(result2);
-  test('{[](})(){{}[]}')
-
-
+  //функция проверки валидности полученой строки
   
-  
+  function testThis(result) {
+    let prov = result;
 
+    for (let s = 0; s < result.length; s++) {
+      bracketCouple.forEach(elem => prov = prov.replace(elem, '')) ;
+      if (!prov) {
+        console.log(`Проверка окончена, строка ${result} валидна.`);
+        break
+      } 
+    }
+    if (prov) {
+      console.log(`Строка ${result} не валидна, остаток ${prov}`);
+    }
+  }
+
+  // Варианты решения. с использованием усложнений.
+
+  // let result1 = generator1(n);
+  // test(result1);
+
+  // let result2 = generator2(n);
+  // test(result2);
+  
 })()
--- JS/test.js
+++ JS/test.js
@@ -5,13 +5,10 @@ export function test(result) {
 
   for (let s = 0; s < result.length; s++) {
     bracketCouple.forEach(elem => prov = prov.replace(elem, '')) ;
-    // console.log('step ', s, prov);
     if (!prov) {
       console.log(`Проверка окончена, строка ${result} валидна.`);
       break
     } 
-    
-    
   }
   if (prov) {
     console.log(`Строка ${result} не валидна, остаток ${prov}`);
